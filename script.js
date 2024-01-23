ad506f86ec894b5ed8510db76b71abf4cbbe1f70
martin <dass52120@gmail.com>
2024-01-23 16:58:41 +0530

    new hover

---
+++ script.js
@@ -0,0 +1,12 @@
+Shery.imageEffect("#back", {style: 5,gooey:true,
+   config: {"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.792531029537418},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":2.12,"range":[0.1,5]},"durationIn":{"value":0.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.99,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.49,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":15.7,"range":[0,100]}}
+
+
+    
+})
+
+
+// document.querySelector(".main")
+// .addEventListener("click", function(){
+//     alert();
+// })
